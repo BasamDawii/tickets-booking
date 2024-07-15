1e960bcdd68c9829b82ff6d1ab58dd09064a9d8b
BasamDawii <91468338+BasamDawii@users.noreply.github.com>
2024-07-15 22:23:01 +0200

    refactor the project with new logic that serves the requirement for the re-exam

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,6 +1,80 @@
+using System.Reflection;
+using Fleck;
+using lib;
+
+
 var builder = WebApplication.CreateBuilder(args);
+var clientEventHandlers = builder.FindAndInjectClientEventHandlers(Assembly.GetExecutingAssembly());
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+int totalTickets = 100; 
+int ticketsSold = 0;
+double basePrice = 50.0; 
+double priceIncreasePerTicket = 2.0; 
+List<IWebSocketConnection> allSockets = new List<IWebSocketConnection>();
+
+
+var server = new WebSocketServer("ws://0.0.0.0:8181");
+
+server.Start(socket =>
+{
+    socket.OnOpen = () =>
+    {
+        Console.WriteLine("Open!");
+        allSockets.Add(socket);
+        SendPriceUpdate(socket);
+    };
+    socket.OnClose = () =>
+    {
+        Console.WriteLine("Close!");
+        allSockets.Remove(socket);
+    };
+    socket.OnMessage = message =>
+    {
+        Console.WriteLine(message);
+        if (message == "buy")
+        {
+            BuyTicket();
+            BroadcastPriceUpdate();
+        }
+    };
+});
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
+
+void BuyTicket()
+{
+    if (ticketsSold < totalTickets)
+    {
+        ticketsSold++;
+        Console.WriteLine($"Ticket sold! Total sold: {ticketsSold}");
+    }
+}
+
+
+void BroadcastPriceUpdate()
+{
+    var currentPrice = CalculateCurrentPrice();
+    foreach (var socket in allSockets)
+    {
+        socket.Send($"Current Price: {currentPrice:C}");
+    }
+}
+
+
+double CalculateCurrentPrice()
+{
+    return basePrice + (ticketsSold * priceIncreasePerTicket);
+}
+
+
+void SendPriceUpdate(IWebSocketConnection socket)
+{
+    var currentPrice = CalculateCurrentPrice();
+    socket.Send($"Current Price: {currentPrice:C}");
+}
 
 app.Run();
--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da55647569ba3aeff8fd980ab5947fdf58129b5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
